32e68ef6063578ba216aba50b0ced6018d207217
Doyi Kim <doyi.kim@tapjoy.com>
2021-07-20 13:17:18 +0900

    modified samples for test

--- index.js
+++ index.js
@@ -1,66 +1,72 @@
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const logger = require('morgan');
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
 
 const app = express();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-if (process.env.NODE_ENV === 'production') app.use(logger('tiny'));
-else app.use(logger('dev'));
+if (process.env.NODE_ENV === "production") app.use(logger("tiny"));
+else app.use(logger("dev"));
 
-app.get('/', (req, res, next) => res.send('API-TEST-APP'));
+app.get("/", (req, res, next) => res.send("API-TEST-APP"));
 
 // single wrapper
-app.get('/inline-linear', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/public/inline-linear.xml'));
+app.get("/inline-linear", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/public/inline-linear.xml"));
 });
 
 // multiple wrapper
-app.get('/multiple-wrapper', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/public/d1.xml'));
+app.get("/multiple-wrapper", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/public/d1.xml"));
 });
 
-app.get('/d2', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/public/d2.xml'));
+app.get("/d2", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/public/d2.xml"));
 });
 
-app.get('/d3', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/public/d3.xml'));
+app.get("/d3", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/public/d3.xml"));
 });
 
-app.get('/d4', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/public/d4.xml'));
+app.get("/d4", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/public/d4.xml"));
 });
 
-app.get('/d5', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/public/d5.xml'));
+app.get("/d5", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/public/d5.xml"));
 });
 
-app.get('/d6', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/public/d6.xml'));
+app.get("/d6", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/public/d6.xml"));
 });
 
-app.get('/d7', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/public/d7.xml'));
+app.get("/d7", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/public/d7.xml"));
 });
 
-app.get('/d8', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/public/d8.xml'));
+app.get("/d8", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/public/d8.xml"));
 });
 
-app.get('/d9', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/public/d9.xml'));
+app.get("/d9", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/public/d9.xml"));
+});
+
+app.get("/d10", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/public/d10.xml"));
 });
 
 // companion click tracking test
-app.get('/cct', (req, res, next) => {
-  console.log('companion click tracking');
+app.get("/cct", (req, res, next) => {
+  console.log("companion click tracking");
   res.end();
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server running on ${port}, http://localhost:${port}`)
+);
