04c5f5ea2e0a93bc6cc0c08b495743595e96ecd1
Hyeonchang Shin <shinhc5@gmail.com>
2021-07-20 16:38:42 +0900

    feat: add multiple companion into wrappers

--- index.js
+++ index.js
@@ -66,6 +66,11 @@ app.get('/cct/:id', (req, res, next) => {
   res.end();
 });
 
+app.get('/cct/:id/:sub_id', (req, res, next) => {
+  console.log('companion click tracking', `id: ${req.params.id}-${req.params.sub_id}`);
+  res.end();
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
